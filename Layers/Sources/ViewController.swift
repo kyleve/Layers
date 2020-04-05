92eecc46ab400b6deb1821972275fee370733494
Kyle Van Essen <k@squareup.com>
2020-04-05 15:39:47 -0700

    more

--- Layers/Sources/ViewController.swift
+++ Layers/Sources/ViewController.swift
@@ -7,36 +7,43 @@
 
 import UIKit
 
-final class Container {
-    
-    var root : ViewController? {
-        didSet {
-            guard self.root !== oldValue else {
-                return
-            }
-        }
-    }
-    
-    let host : UIViewController
-    
-    init(root : ViewController? = nil, host : UIViewController) {
-        self.root = root
-        self.host = host
-    }
-    
-    private func updateRoot(from : ViewController?, to : ViewController?) {
-        
-    }
-}
 
-
-final class ViewController {
+final class ViewControllerContainer<ViewControllerType : UIViewController> : AnyViewControllerContainer {
     
     var presentationStyle : PresentationStyle
     
-    var viewController : UIViewController
+    var viewController : ViewControllerType
     
-    var child : ViewController?
+    var child : AnyViewControllerContainer?
+    
+    init(
+        presentationStyle : PresentationStyle,
+        viewController : ViewControllerType,
+        child : AnyViewControllerContainer? = nil
+    ) {
+        self.presentationStyle = presentationStyle
+        self.viewController = viewController
+        self.child = child
+    }
+    
+    // MARK: AnyViewControllerContainer
+    
+    var anyViewController : UIViewController {
+        self.viewController
+    }
+    
+    var anyViewControllerType : UIViewController.Type {
+        ViewControllerType.self
+    }
+}
+
+protocol AnyViewControllerContainer {
+    var presentationStyle : PresentationStyle { get }
+    
+    var anyViewController : UIViewController { get }
+    var anyViewControllerType : UIViewController.Type { get }
+    
+    var child : AnyViewControllerContainer? { get }
 }
 
 
@@ -70,4 +77,106 @@ final class PositionProvider {
     }
     
     var setNeededUpdate : ((PositionProvider) -> ())? = nil
+}
+
+
+struct ViewControllerTree {
+    
+    var root : Node
+    
+    struct Node {
+        var provider : () -> AnyViewControllerContainer
+        var viewControllerType : UIViewController.Type
+        var child : Child
+        
+        init<ViewControllerType:UIViewController>(
+            provider : @escaping () -> ViewControllerContainer<ViewControllerType>,
+            child : Child
+        ) {
+            self.viewControllerType = ViewControllerType.self
+            
+            self.provider = provider
+            self.child = child
+        }
+        
+        func toIdentified(with depth : Int = 0) -> IdentifiedViewControllerTree.Node {
+            .init(
+                provider: self.provider,
+                viewControllerType: self.viewControllerType,
+                identifier: .init(
+                    viewControllerType: self.viewControllerType,
+                    depth: depth
+                ),
+                child: self.child.node.flatMap { .child($0.toIdentified(with: depth + 1)) } ?? .none
+            )
+        }
+    }
+    
+    indirect enum Child {
+        case none
+        case child(Node)
+        
+        var node : Node? {
+            switch self {
+            case .none: return nil
+            case .child(let node): return node
+            }
+        }
+    }
+}
+
+struct IdentifiedViewControllerTree {
+    
+    var root : Node
+    
+    struct Node {
+        var provider : () -> AnyViewControllerContainer
+        var viewControllerType : UIViewController.Type
+        var identifier : Identifier
+        var child : Child
+        
+        func flattened() -> [FlatNode] {
+            var flattened = [FlatNode]()
+            
+            self.flatten(into: &flattened)
+            
+            return flattened
+        }
+        
+        private func flatten(into flattened : inout [FlatNode]) {
+            
+            flattened.append(FlatNode(
+                provider: self.provider,
+                viewControllerType: self.viewControllerType,
+                identifier: self.identifier
+            ))
+            
+            if let child = self.child.node {
+                child.flatten(into: &flattened)
+            }
+        }
+    }
+    
+    struct FlatNode {
+        var provider : () -> AnyViewControllerContainer
+        var viewControllerType : UIViewController.Type
+        var identifier : Identifier
+    }
+    
+    indirect enum Child {
+        case none
+        case child(Node)
+        
+        var node : Node? {
+            switch self {
+            case .none: return nil
+            case .child(let node): return node
+            }
+        }
+    }
+    
+    struct Identifier {
+        var viewControllerType : UIViewController.Type
+        var depth : Int
+    }
 }
--- Layers/Tests/Tests.swift
+++ Layers/Tests/Tests.swift
@@ -6,7 +6,49 @@
 //
 
 import XCTest
+@testable import Layers
+
 
 final class Tests : XCTestCase {
     
+    func test_tree() {
+        let tree = ViewControllerTree(
+            root: .init(provider: {
+                .init(
+                    presentationStyle: .init(positioning: .relativeToWindow),
+                    viewController: VC_A()
+                )
+            }, child: .child(.init(
+                provider: {
+                    .init(
+                        presentationStyle: .init(positioning: .relativeToWindow),
+                        viewController: VC_B()
+                    )
+            }, child: .child(.init(
+                provider: {
+                    .init(
+                        presentationStyle: .init(positioning: .relativeToWindow),
+                        viewController: VC_C()
+                    )
+            }, child: .child(.init(
+                provider: {
+                    .init(
+                        presentationStyle: .init(positioning: .relativeToWindow),
+                        viewController: VC_D()
+                    )
+            }, child: .none))))))
+            )
+        )
+        
+        let identified = tree.root.toIdentified()
+        
+        print(identified)
+    }
 }
+
+
+fileprivate final class VC_A : UIViewController {}
+fileprivate final class VC_B : UIViewController {}
+fileprivate final class VC_C : UIViewController {}
+fileprivate final class VC_D : UIViewController {}
+fileprivate final class VC_E : UIViewController {}
